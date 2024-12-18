03a35d447fe063506df349900f698f47e5b18e19
Ketsa143 <88353744+Ketsa143@users.noreply.github.com>
2024-12-18 17:13:24 -0500

    Update users.js

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,7 +1,7 @@
 // users.js
 const users = [
-    { username: "Asistente", password: "12345", role: "asistente", redirect: "/roles/asistente.html" },
-    { username: "Admin", password: "Admin123", role: "administrador", redirect: "/roles/administrador.html" },
-    { username: "Contador", password: "Conta2024", role: "contador", redirect: "/roles/contador.html" },
+    { username: "Asistente", password: "12345", role: "asistente", redirect: "./roles/asistente.html" },
+    { username: "Admin", password: "Admin123", role: "administrador", redirect: "./roles/administrador.html" },
+    { username: "Contador", password: "Conta2024", role: "contador", redirect: "./roles/contador.html" },
 ];
 export { users };
